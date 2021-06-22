a246af8644ca3a981ca3ca42e45ffdf8c4adb6d9
finale225 <finale2255@naver.com>
2021-06-22 14:16:34 +0900

    LeapYear

--- comparison.cpp
+++ comparison.cpp
@@ -2,12 +2,9 @@
 using namespace std;
 
 int main() {
-    int score;
-    cin >> score;
-    if (score > 100 || score < 0) return 0;
-    if (score >= 90 && score <= 100) cout << 'A';
-    else if (score >= 80) cout << 'B';
-    else if (score >= 70) cout << 'C';
-    else if (score >= 60) cout << 'D';
-    else cout << 'F';
+    int year;
+    cin >> year;
+    if (year % 4 == 0 && year % 100 != 0) cout << true;
+    else if (year % 400 == 0) cout << true;
+    else cout << false;
 }
