984d43e051bebe2582d32024c5639196b6bad400
finale225 <finale2255@naver.com>
2021-06-22 14:09:40 +0900

    Score

--- comparison.cpp
+++ comparison.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    cin >> a >> b;
-    if (a > b) cout << '>';
-    else if (a == b) cout << '=';
-    else cout << '<';
+    int score;
+    cin >> score;
+    if (score > 100 || score < 0) return 0;
+    if (score >= 90 && score <= 100) cout << 'A';
+    else if (score >= 80) cout << 'B';
+    else if (score >= 70) cout << 'C';
+    else if (score >= 60) cout << 'D';
+    else cout << 'F';
 }
